d742b0713c9ed26fb6855fbe087fb5cba01cf145
vmarchesin <dev@vmarches.in>
2022-04-22 20:12:30 +0200

    ✨ OpenGraph metadata

--- components/head/head.tsx
+++ components/head/head.tsx
@@ -9,6 +9,23 @@ const Head: React.FC = () => {
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+      <meta name="description" content="Vinicius Marchesin Araujo - Engineering Manager & Developer" />
+
+
+      <meta property="og:url" content="https://vmarches.in/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="vmarches.in" />
+      <meta property="og:description" content="Vinicius Marchesin Araujo - Engineering Manager & Developer" />
+      <meta property="og:image" content="https://vmarches.in/og-share.png" />
+
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="vmarches.in" />
+      <meta property="twitter:url" content="https://vmarches.in/" />
+      <meta name="twitter:title" content="vmarches.in" />
+      <meta name="twitter:description" content="Vinicius Marchesin Araujo - Engineering Manager & Developer" />
+      <meta name="twitter:image" content="https://vmarches.in/og-share-twitter.png" />
     </NextHead>
   );
 };
